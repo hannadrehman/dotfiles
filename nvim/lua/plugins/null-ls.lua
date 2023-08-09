52e015bdcd26fd30feed57234d676b5a626111c2
hannad rehman <>
2023-08-09 16:38:47 +0530

    updates null ls

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -5,6 +5,7 @@ require("null-ls").setup({
         require("null-ls").builtins.formatting.prettier,
         require("null-ls").builtins.diagnostics.eslint,
         require("null-ls").builtins.completion.spell,
+	require("null_ls").builtins.diagnostics.stylelint
     },
     on_attach = function(client)
     if client.server_capabilities.documentFormattingProvider then
