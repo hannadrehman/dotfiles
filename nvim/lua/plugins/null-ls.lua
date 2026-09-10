d9e9902f5144e3cc9563578df0d08cb4a2daf4a1
hannad rehman <>
2026-09-10 22:48:53 +0530

    fixes formatting

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -6,7 +6,7 @@ require("null-ls").setup({
         require("null-ls").builtins.formatting.prettier,
         require("none-ls.diagnostics.eslint"),
         require("null-ls").builtins.completion.spell,
-	require("null-ls").builtins.diagnostics.stylelint
+	      require("null-ls").builtins.diagnostics.stylelint
     },
     on_attach = function(client)
     if client.server_capabilities.documentFormattingProvider then
